3ac678679cee6763a836cfd4104c443d87c7a9a4
absarker <absarker238@gmail.com>
2025-02-21 03:51:49 +0700

    added my players section

--- src/Player/Player.jsx
+++ src/Player/Player.jsx
@@ -1,9 +1,38 @@
-const Player = () => {
+import PropTypes from "prop-types";
+import { TiFlag } from "react-icons/ti";;
+const Player = ({ player }) => {
+    const { name, price, img, batting_style, country } = player
     return (
-        <div>
-            
+        <div className="rounded-2xl border-1">
+            <div className="box-border  w-96 h-60  p-5 ">
+                <img className="w-full h-full object-cover bg-center rounded-2xl" src={img} alt="" />
+            </div>
+            <div>
+                <div className="flex flex-col">
+                    <h1>{name}</h1>
+                    <div className=" flex justify-between px-1 items-center">
+                        <h3 className="flex  justify-between items-center text-[16px] text-gray-500  "> <TiFlag className="text-2xl"/> {country}</h3>
+                        <button className="btn p-2 border-2 rounded-xl border-gray-200">All-Rounder</button>
+                    </div>
+                    <hr />
+
+                </div>
+       
+                <div className="flex flex-col">
+                   <h1 className="border-3 text-start">Rating</h1>
+                   <div className="grid grid-cols-2">
+                    <p>{batting_style}</p>
+                    <p>{batting_style}</p>
+                    <p>Price:${price}</p>
+                    <button>Choose Player</button>
+                   </div>
+                </div>
+            </div>
+
         </div>
     );
 };
-
+Player.propTypes = {
+    player: PropTypes.object.isRequired,
+}
 export default Player;
--- src/Players/Players.jsx
+++ src/Players/Players.jsx
@@ -1,8 +1,17 @@
+import { useEffect, useState } from "react";
+import Player from "../Player/Player";
 const Players = () => {
+    const [players, setPlayers]=useState([])
+    useEffect(()=>{
+        fetch('Fake data.json')
+        .then(res=>res.json())
+        .then(data=>setPlayers(data))
+    },[])
+console.log(players)
     return (
-        <div>
+       <div className="grid grid-cols-3 border-4 mx-10 px-auto gap-6">
            {
-            
+            players.map(player=><Player key={player.id} player={player}></Player>)
            }
         </div>
     );
